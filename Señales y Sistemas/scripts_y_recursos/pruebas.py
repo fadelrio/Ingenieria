a18f308c2842cbba6d25281979118a3cf887565d
fadelrio <delriofrancisco03@gmail.com>
2024-12-06 19:42:52 -0300

    no tengo ni idea de que es

--- Señales y Sistemas/scripts_y_recursos/pruebas.py
+++ Señales y Sistemas/scripts_y_recursos/pruebas.py
@@ -3,51 +3,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# sample_rate = 100
+sample_rate = 100
+tiempo = np.arange(-5, 5, 1/sample_rate)
 #
-# tiempo = np.arange(-5, 5, 1/sample_rate)
+w = 2*np.pi
 #
-# w = 2*np.pi
-#
-# #senal = np.cos(w*tiempo)
+senal = np.cos(w*tiempo)*np.cos(w*tiempo)*np.cos(2*w*tiempo)*np.cos(2*w*tiempo)
 #
 # long_escalon = int(len(tiempo)/3)
 # long_padding = int(len(tiempo)/3)
 #
 # senal = np.concatenate(np.zeros(long_padding),np.ones(long_escalon),np.zeros(long_padding))
 #
-# fft = np.fft.fft(senal)
-# f_fft = np.fft.fftfreq(len(senal), 1/sample_rate)
+fft = np.fft.fft(senal)
+f_fft = np.fft.fftfreq(len(senal), 1/sample_rate)
 #
-# frecuencias = f_fft[:len(senal) // 2]
-# fft_signal = np.abs(fft[:len(senal) // 2])
+frecuencias = f_fft[:len(senal) // 2]
+fft_signal = np.abs(fft[:len(senal) // 2])
 #
-# plt.figure()
-# plt.plot(frecuencias, fft_signal)
-#
-#
-# plt.figure()
-#
-# plt.plot(tiempo, senal)
-#
-# plt.show()
+plt.figure()
+plt.plot(frecuencias, fft_signal)
+
+
+plt.figure()
+
+plt.plot(tiempo, senal)
+
+plt.show()
 
 
 #u(t)
-senal = np.concatenate([np.zeros(40000), np.ones(100), np.zeros(40000)])
+#senal = np.concatenate([np.zeros(40000), np.ones(100), np.zeros(40000)])
 
-sample_rate=48000
+#sample_rate=48000
 
-tiempo = np.arange(len(senal))/sample_rate
+#tiempo = np.arange(len(senal))/sample_rate
 
-fft = np.fft.fft(senal)
-f_fft = np.fft.fftfreq(len(senal), 1/sample_rate)
+#fft = np.fft.fft(senal)
+#f_fft = np.fft.fftfreq(len(senal), 1/sample_rate)
 
-frecuencias = f_fft[:len(senal) // 2]
-fft_signal = np.abs(fft[:len(senal) // 2])
+#frecuencias = f_fft[:len(senal) // 2]
+#fft_signal = np.abs(fft[:len(senal) // 2])
 
-plt.figure()
-plt.plot(frecuencias, fft_signal)
+#plt.figure()
+#plt.plot(frecuencias, fft_signal)
 
 
 plt.figure()
